5324027213f31c92752f861248b4df258d827437
ndjerma <138213492+ndjerma@users.noreply.github.com>
2025-05-13 11:01:16 +0200

    kom

--- src/main/java/com/example/demo/models/UserModel.java
+++ src/main/java/com/example/demo/models/UserModel.java
@@ -10,4 +10,7 @@ public class UserModel {
 
         // da ne bi pravili getere i setere koristimo u UserControlleru anotaciju @RequestBody koja instancira
         // ceo user model i prima podatke bez da se mi jebavamo rucno
+
+    //dodao sam komentar
+
 }
